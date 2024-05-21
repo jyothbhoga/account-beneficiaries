a69428de2cbf5a1f3b687e030bff348ef97d6bec
Jyoth <77140785+jyothbhoga@users.noreply.github.com>
2024-05-21 13:09:39 +0530

    fix: adde a fix

--- src/components/ViewBeneficiary/index.js
+++ src/components/ViewBeneficiary/index.js
@@ -32,6 +32,7 @@ const ViewBenificiary = memo((props) => {
       );
       setCurrBeneficiary(currBenefT);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return currBeneficiary ? (
